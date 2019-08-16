1cc4bb02687b251323c464a16064d240011222c1
2p-offer <1102568707@qq.com>
2019-08-16 16:05:54 +0800

    TestCommit

--- src/main/java/treadSelf/subAndMain/SubAndMain.java
+++ src/main/java/treadSelf/subAndMain/SubAndMain.java
@@ -1,13 +1,20 @@
 package treadSelf.subAndMain;
-
 /**
  * Created by 2P on 18-12-12.
  */
 public class SubAndMain {
     public static void main(String[] args) {
+
+        Sub sub = new Sub();
         SubAndMain subAndMain=new SubAndMain();
         Thread t1 = new Thread(() -> {
-            Sub sub = new Sub();
+            try {
+                sub.subOut();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread t2 = new Thread(() -> {
             try {
                 sub.subOut();
             } catch (InterruptedException e) {
@@ -15,17 +22,16 @@ public class SubAndMain {
             }
         });
         t1.start();
+        t2.start();
     }
-
     static class Main {
         int i = 10;
-        public synchronized void mainOut() throws InterruptedException {
+        public synchronized  void mainOut() throws InterruptedException {
             i--;
             System.out.println("main out :" + i);
             Thread.sleep(100);
         }
     }
-
     static class Sub extends Main {
         public synchronized void subOut() throws InterruptedException {
             while (i > 0) {
@@ -36,5 +42,4 @@ public class SubAndMain {
             }
         }
     }
-
 }
